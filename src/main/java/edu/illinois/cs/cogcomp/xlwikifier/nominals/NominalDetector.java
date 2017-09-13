711464763b69cd23b1a02fe1234855cf0e375385
b29308188 <b29308188@gmail.com>
2017-09-13 17:05:43 -0500

    fix nominal prediction

--- src/main/java/edu/illinois/cs/cogcomp/xlwikifier/nominals/NominalDetector.java
+++ src/main/java/edu/illinois/cs/cogcomp/xlwikifier/nominals/NominalDetector.java
@@ -1,46 +1,52 @@
 package edu.illinois.cs.cogcomp.xlwikifier.nominals;
 
 import edu.illinois.cs.cogcomp.annotation.Annotator;
-import edu.illinois.cs.cogcomp.core.constants.Language;
+import edu.illinois.cs.cogcomp.annotation.AnnotatorConfigurator;
+import edu.illinois.cs.cogcomp.core.datastructures.textannotation.Constituent;
 import edu.illinois.cs.cogcomp.core.datastructures.textannotation.Sentence;
 import edu.illinois.cs.cogcomp.core.datastructures.textannotation.SpanLabelView;
 import edu.illinois.cs.cogcomp.core.datastructures.textannotation.TextAnnotation;
+import edu.illinois.cs.cogcomp.core.utilities.configuration.Configurator;
 import edu.illinois.cs.cogcomp.core.utilities.configuration.ResourceManager;
-import edu.illinois.cs.cogcomp.lbjava.nlp.Word;
+import edu.illinois.cs.cogcomp.lbjava.learn.Lexicon;
 import edu.illinois.cs.cogcomp.lbjava.parse.LinkedVector;
-import edu.illinois.cs.cogcomp.ner.ExpressiveFeatures.BrownClusters;
 import edu.illinois.cs.cogcomp.ner.ExpressiveFeatures.ExpressiveFeaturesAnnotator;
-import edu.illinois.cs.cogcomp.ner.ExpressiveFeatures.Gazetteers;
-import edu.illinois.cs.cogcomp.ner.ExpressiveFeatures.GazetteersFactory;
 import edu.illinois.cs.cogcomp.ner.InferenceMethods.Decoder;
 import edu.illinois.cs.cogcomp.ner.LbjFeatures.NETaggerLevel1;
 import edu.illinois.cs.cogcomp.ner.LbjFeatures.NETaggerLevel2;
-import edu.illinois.cs.cogcomp.ner.LbjTagger.Data;
-import edu.illinois.cs.cogcomp.ner.LbjTagger.NERDocument;
-import edu.illinois.cs.cogcomp.ner.LbjTagger.NEWord;
-import edu.illinois.cs.cogcomp.ner.LbjTagger.ParametersForLbjCode;
+import edu.illinois.cs.cogcomp.ner.LbjTagger.*;
 import edu.illinois.cs.cogcomp.ner.NERAnnotator;
-import edu.illinois.cs.cogcomp.ner.ParsingProcessingData.TaggedDataReader;
 import edu.illinois.cs.cogcomp.ner.config.NerBaseConfigurator;
-import edu.illinois.cs.cogcomp.xlwikifier.ConfigParameters;
+import edu.illinois.cs.cogcomp.ner.config.NerOntonotesConfigurator;
+import edu.illinois.cs.cogcomp.xlwikifier.MultiLingualNER;
 import edu.illinois.cs.cogcomp.xlwikifier.datastructures.ELMention;
 import edu.illinois.cs.cogcomp.xlwikifier.datastructures.QueryDocument;
-import edu.illinois.cs.cogcomp.xlwikifier.mlner.NERUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.*;
 
-import static edu.illinois.cs.cogcomp.ner.LbjTagger.Parameters.readAndLoadConfig;
-
+/**
+ * Created by lchen112 on 9/12/2017.
+ */
 /**
  * Created by lchen112 on 9/12/2017.
  */
 public class NominalDetector extends NERAnnotator {
-    ResourceManager rm;
     public NominalDetector(String configFile) throws IOException {
         super(configFile, "NOM");
         doInitialize();
+    }
+    public void annotate(QueryDocument doc) {
+        TextAnnotation ta = doc.getTextAnnotation();
+        addView(ta);
+        for (Constituent c : ta.getView(this.viewName).getConstituents()) {
+            ELMention m = new ELMention(doc.getDocID(), c.getStartCharOffset(), c.getEndCharOffset());
+            m.setSurface(c.getSurfaceForm());
+            m.setType(c.getLabel());
+            m.setNounType("NOM");
+            doc.mentions.add(m);
+        }
     }
 }
